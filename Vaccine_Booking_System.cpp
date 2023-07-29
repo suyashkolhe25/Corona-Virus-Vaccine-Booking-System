b3205ab90af2cd3cfbe17b50533da4966c7c3820
Suyash Kolhe <suyashkolhe01@gmail.com>
2023-07-30 01:22:13 +0530

    Added Menu Function

--- Vaccine_Booking_System.cpp
+++ Vaccine_Booking_System.cpp
@@ -1,6 +1,19 @@
 #include<iostream>
 using namespace std;
 
+void menu(){
+    system("cls");
+    cout<<"Welcome to Corona Vaccine Booking System"<<endl;
+    cout<<"================================================="<<endl;
+    cout<<"1. Create Patient record and book vaccine"<<endl;
+    cout<<"2. Show all Vaccine booking record "<<endl;
+    cout<<"3. Search for a specific booking record "<<endl;
+    cout<<"4. Update a booking record"<<endl;
+    cout<<"5. Delete a booking record"<<endl;
+    cout<<"6. Exit"<<endl;
+    cout<<"Enter your choice(1-6): "<<endl;
+}
+
 int main() {
     cout<<"Hello world";
 }
